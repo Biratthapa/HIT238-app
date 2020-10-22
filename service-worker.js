01a4914c7a0285e420390282463fd43745000dfb
Biratthapa <48338951+Biratthapa@users.noreply.github.com>
2020-10-22 22:32:03 +0930

    added fetch

--- service-worker.js
+++ service-worker.js
@@ -21,23 +21,30 @@ self.addEventListener('install', function(e) {
       return cache.addAll(cacheFiles);
     })
   )
-})
+});
 
 self.addEventListener('activate', function(e) {
   console.log('Service Worker:Activated');
   e.waitUntil(
 
-    caches.keys().then(function(cacheNames){
-       return Promise.all(cacheNames.map(function(thisCacheName){
-         if (thisCacheName !== cacheName) {
-           console.log("[ServiceWorker] Removing Cached Files from", thisCacheName);
-           return caches.delete(thisCacheName);
+    caches.keys().then(function(name){
+       Promise.all(
+         names.filter(function(cacheName){
+           return name !== cacheName;
+         }).map(function(name){
+           return caches.delete(name);
+         })
          }
        }))
     })
   )
-})
+});
 
 self.addEventListener('fetch', function(e) {
+  event.respondWith(
+    caches.open(cacheName).then(function(cache){
+      return cache.match(event.request);
+    })
+  );
   console.log('Service Worker: fetching', e.request.url);
-})
+});
